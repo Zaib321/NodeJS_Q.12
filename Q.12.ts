12d4f8ca65244c254f39355fd764bff3d2c241a4
Zaib321 <161988392+Zaib321@users.noreply.github.com>
2024-03-08 07:22:27 +0500

    NodeJS_Q.12

---
+++ Q.12.ts
@@ -0,0 +1,10 @@
+//Q.12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names: string[]= ["Sidra" , "Fahad",  "Mujahid", "Annie" ];
+
+let message:string= "would you like to have cofee with me?";
+
+console.log(names[0] + " "  + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
